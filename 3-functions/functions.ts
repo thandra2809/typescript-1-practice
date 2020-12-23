34f78ea41b1ee28827fb5cac72a90e6862cdc6f8
Srinivasa Rao THANDRA <thandra82@gmail.com>
2020-12-23 00:37:27 -0800

    added some sample files

--- 3-functions/functions.ts
+++ 3-functions/functions.ts
@@ -11,4 +11,25 @@ const throwError = (message: string): never => {
     throw new Error(message);
 }
 
-console.log(throwError('This is error occurred while learning typescript'));
+// console.log(throwError('This is error occurred while learning typescript'));
+
+
+const todayWeather = {
+    date: new Date(),
+    weather: 'sunny'
+}
+
+const logWeather = (forecast: { date: Date, weather: string }): void => {
+    console.log(forecast.date);
+    console.log(forecast.weather);
+}
+
+logWeather(todayWeather)
+
+
+const logWeatherDestructured = ({date, weather}: { date: Date, weather: string }): void => {
+    console.log(date);
+    console.log(weather);
+}
+
+logWeatherDestructured(todayWeather)
