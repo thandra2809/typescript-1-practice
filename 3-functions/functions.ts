74bdf8a2a5bd643faf90d68438750916e7b2c687
Srinivasa Rao THANDRA <thandra82@gmail.com>
2020-12-22 22:35:52 -0800

    added some sample files

--- 3-functions/functions.ts
+++ 3-functions/functions.ts
@@ -3,6 +3,12 @@ const add = (a: number, b: number): number => {
 }
 
 
-console.log('Added value is: ',add(3,4))
+console.log('Added value is: ', add(3, 4))
 
 /** never type for functions that is throwing errors **/
+
+const throwError = (message: string): never => {
+    throw new Error(message);
+}
+
+console.log(throwError('This is error occurred while learning typescript'));
