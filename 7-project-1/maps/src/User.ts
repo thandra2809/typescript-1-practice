634f705be21e86c3f84810fdb1a0b7180937d617
Srinivasa Rao THANDRA <thandra82@gmail.com>
2020-12-25 23:15:12 -0800

    added some sample files

--- 7-project-1/maps/src/User.ts
+++ 7-project-1/maps/src/User.ts
@@ -1,11 +1,19 @@
-class User {
+// @ts-ignore
+import faker from 'faker';
+
+export class User {
     name: string;
     location: {
         lat: number;
         lng: number;
     };
 
-    constructor(){
+    constructor() {
+        this.name = faker.name.findName(1);
+        this.location = {
+            lat: parseFloat(faker.address.latitude()),
+            lng: parseFloat(faker.address.longitude())
+        }
 
     }
 
--- 7-project-1/maps/src/index.ts
+++ 7-project-1/maps/src/index.ts
@@ -1,1 +1,4 @@
-console.log('Hi There')
+new google.maps.Map(document.getElementById("map"), {
+    zoom: 1,
+    center: {lat: 0, lng: 0}
+});
