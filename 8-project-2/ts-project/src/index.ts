c0347f0df9343e95dec998c3414ca0d20e1e09ad
Srinivasa Rao THANDRA <thandra82@gmail.com>
2020-12-26 22:52:13 -0800

    added some sample files

--- 8-project-2/ts-project/src/index.ts
+++ 8-project-2/ts-project/src/index.ts
@@ -1,1 +1,34 @@
-console.log('This is first time using auto compilation');
+class Sorter {
+
+    constructor(public collection: number[] | string) {
+    }
+
+    sort(): void {
+
+        const {length} = this.collection;
+
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i - 1; j++) {
+
+                if (this.collection instanceof Array) {
+                    // Type of collection is Array
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        const temp = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        // String Implementation
+        if(typeof this.collection ==='string'){
+
+        }
+
+    }
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection)
